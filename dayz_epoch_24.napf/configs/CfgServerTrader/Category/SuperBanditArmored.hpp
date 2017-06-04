73af5635e7b2a29e3b66a28563e70061f6b7dbe9
jimmypesola <jimmy@pesola.eu>
2017-06-04 18:49:36 +0200

    Fix super hero/bandit armored category
    Changed from the incorrect type trade_items to
    trade_any_vehicle.
    
    Changed M113 Ambulance type from UN to TK

--- dayz_epoch_24.napf/configs/CfgServerTrader/Category/SuperBanditArmored.hpp
+++ dayz_epoch_24.napf/configs/CfgServerTrader/Category/SuperBanditArmored.hpp
@@ -1,46 +1,46 @@
 class Category_694 {
 	class M1133_MEV_EP1 {
-		type = "trade_items";
+		type = "trade_any_vehicle";
 		buy[] = {2,"ItemAmethyst"};
 		sell[] = {1,"ItemTopaz"};
 	};
 	class M113_TK_EP1 {
-		type = "trade_items";
+		type = "trade_any_vehicle";
 		buy[] = {1,"ItemCitrine"};
 		sell[] = {1,"ItemObsidian"};
 	};
-	class M113Ambul_UN_EP1 {
-		type = "trade_items";
+	class M113Ambul_TK_EP1 {
+		type = "trade_any_vehicle";
 		buy[] = {4,"ItemObsidian"};
 		sell[] = {4,"ItemBriefcase100oz"};
 	};
 	class BTR90_HQ_DZE {
-		type = "trade_items";
+		type = "trade_any_vehicle";
 		buy[] = {3,"ItemAmethyst"};
 		sell[] = {1,"ItemTopaz"};
 	};
 	class LAV25_HQ_DZE {
-		type = "trade_items";
+		type = "trade_any_vehicle";
 		buy[] = {3,"ItemAmethyst"};
 		sell[] = {1,"ItemTopaz"};
 	};
 	class M1126_ICV_M2_EP1 {
-		type = "trade_items";
+		type = "trade_any_vehicle";
 		buy[] = {1,"ItemCitrine"};
 		sell[] = {1,"ItemObsidian"};
 	};
 	class M1126_ICV_mk19_EP1 {
-		type = "trade_items";
+		type = "trade_any_vehicle";
 		buy[] = {2,"ItemCitrine"};
 		sell[] = {2,"ItemObsidian"};
 	};
 	class AAV {
-		type = "trade_items";
+		type = "trade_any_vehicle";
 		buy[] = {3,"ItemCitrine"};
 		sell[] = {1,"ItemSapphire"};
 	};
 	class BAF_Jackal2_L2A1_w {
-		type = "trade_items";
+		type = "trade_any_vehicle";
 		buy[] = {6,"ItemObsidian"};
 		sell[] = {1,"ItemBriefcase100oz"};
 	};
