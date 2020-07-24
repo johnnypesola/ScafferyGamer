c2650f09e1efafd6d397587f805aff8b544145f7
Jimmy Pesola <jimmy@pesola.eu>
2020-07-24 17:14:51 +0200

    Add tooltips to gems
    Add tooltips to gems which show their value
    in briefcases with 100oz gold.

--- dayz_epoch_11.chernarus/custom/extra_rc/extra_rc.hpp
+++ dayz_epoch_11.chernarus/custom/extra_rc/extra_rc.hpp
@@ -175,281 +175,50 @@ class ExtraRc {
    script = "[[""5Rnd_127x108_KSVK""],[""50Rnd_127x107_DSHKM""]] execVM ""custom\ammo_tools\player_convertMags.sqf"";";
   };
  };
-
+ class ItemRuby {
+   class worth {
+    text = "5000 BC";
+    script = "";
+   };
+ };
+ class ItemCitrine {
+  class worth {
+   text = "1000 BC";
+   script = "";
+  };
+ };
+ class ItemEmerald {
+  class worth {
+   text = "500 BC";
+   script = "";
+  };
+ };
+ class ItemAmethyst {
+  class worth {
+   text = "100 BC";
+   script = "";
+  };
+ };
+ class ItemSapphire {
+  class worth {
+   text = "50 BC";
+   script = "";
+  };
+ };
  class ItemObsidian {
+  class worth {
+   text = "10 BC";
+   script = "";
+  };
   class buildBed {
    text = "Build Spawn Bed";
    script = "[""MAP_F_postel_manz_kov"",[""ItemToolbox"",""ItemCrowbar"",""ItemHatchet""],[[""ItemObsidian"", 1],[""PartGeneric"", 1],[""PartWoodLumber"", 5]],[-1.0,1.5,0.05]] execVM ""custom\build\player_customBuild.sqf"";";
   };
  };
-// class ItemRuby {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };    
-// class ItemEmerald {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };    
-// class ItemSapphire {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };    
-// class ItemTopaz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };    
-// class ItemGoldBar {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };    
-// class ItemGoldBar2oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemGoldBar3oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemGoldBar4oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemGoldBar5oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemGoldBar6oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemGoldBar7oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemGoldBar8oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemGoldBar9oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemGoldBar10oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemSilverBar {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemSilverBar2oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemSilverBar3oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemSilverBar4oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemSilverBar5oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemSilverBar6oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemSilverBar7oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemSilverBar8oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemSilverBar9oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemSilverBar10oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemBriefcaseEmpty {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemBriefcase10oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemBriefcase20oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemBriefcase30oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemBriefcase40oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemBriefcase50oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemBriefcase60oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemBriefcase70oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemBriefcase80oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemBriefcase90oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemBriefcase100oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemBriefcaseS10oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemBriefcaseS20oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemBriefcaseS30oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemBriefcaseS40oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemBriefcaseS50oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemBriefcaseS60oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemBriefcaseS70oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemBriefcaseS80oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemBriefcaseS90oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
-// class ItemBriefcaseS100oz {
-//  class Combine {
-//   text = "COMBINE ALL";
-//   script = "execVM 'custom\currency\combineCurrency.sqf'";
-//  };
-// };
+ class ItemTopaz {
+  class worth {
+   text = "5 BC";
+   script = "";
+  };
+ };
 };
